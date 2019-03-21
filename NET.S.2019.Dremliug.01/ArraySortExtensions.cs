213442d1da35a2cce55abdb5153f8da975e1d7f7
deNoor <denoorx@gmail.com>
2019-03-21 19:44:49 +0300

    Initial commit

---
+++ NET.S.2019.Dremliug.01/ArraySortExtensions.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Dremliug._01
+{
+    /// <summary> Provides extension methods for sorting an integer array. </summary>
+    public static class ArraySortExtensions
+    {
+        #region MergeSort
+
+        /// <summary> Sorts with Quick sort algorithm. </summary>
+        /// <param name="array"></param>
+        public static void MergeSort(this int[] array)
+        {
+            #region Validation
+
+            if (array is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (array.Length == 1)
+            {
+                return;
+            }
+
+            #endregion
+
+            #region Initialization
+
+            // Queue will contain sorted arrays.
+            Queue<int[]> partsToMerge = new Queue<int[]>();
+
+            // Split given array into single element arrays. Single element array is sorted.
+            foreach (var v in array)
+            {
+                partsToMerge.Enqueue(new int[] { v });
+            }
+
+            #endregion
+
+            #region Processing
+
+            while (partsToMerge.Count > 1)
+            {
+                // Take two arrays from the queue and create new array for sorted data.
+                int[] partA = partsToMerge.Dequeue();
+                int indexA = 0;
+                int lengthA = partA.Length;
+
+                int[] partB = partsToMerge.Dequeue();
+                int indexB = 0;
+                int lengthB = partB.Length;
+
+                int[] mergeResult = new int[lengthA + lengthB];
+                int indexResult = 0;
+
+                // Merge two arrays into the resulting sorted array.
+                while (indexA < lengthA && indexB < lengthB)
+                {
+                    if (partA[indexA] <= partB[indexB])
+                    {
+                        mergeResult[indexResult] = partA[indexA];
+                        indexA++;
+                    }
+                    else
+                    {
+                        mergeResult[indexResult] = partB[indexB];
+                        indexB++;
+                    }
+
+                    indexResult++;
+                }
+
+                // Copy the remaining elements.
+                while (indexA < lengthA)
+                {
+                    mergeResult[indexResult] = partA[indexA];
+                    indexA++;
+                    indexResult++;
+                }
+
+                // Copy the remaining elements.
+                while (indexB < lengthB)
+                {
+                    mergeResult[indexResult] = partB[indexB];
+                    indexB++;
+                    indexResult++;
+                }
+
+                // Put sorted result back into the queue.
+                partsToMerge.Enqueue(mergeResult);
+            }
+
+            // Now queue contains one sorted array. Fill the original array with sorted data.
+            int[] sorted = partsToMerge.Dequeue();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = sorted[i];
+            }
+
+            #endregion
+        }
+
+        #endregion
+
+        #region QuickSort
+
+        /// <summary> Sorts with Quick sort algorithm. </summary>
+        /// <param name="array"></param>
+        public static void QuickSort(this int[] array)
+        {
+            #region Validation
+
+            if (array is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (array.Length == 1)
+            {
+                return;
+            }
+
+            #endregion
+
+            #region Processing
+
+            QuickSortRecursive(array, 0, array.Length - 1);
+
+            void QuickSortRecursive(int[] arr, int startIndex, int endIndex)
+            {
+                if (startIndex < endIndex)
+                {
+                    // Get pivot index.
+                    int pivotIndex = Partition(arr, startIndex, endIndex);
+
+                    // Sort to the left of the pivot position. 
+                    QuickSortRecursive(arr, startIndex, pivotIndex - 1);
+                    // Sort to the right of the pivot position.
+                    QuickSortRecursive(arr, pivotIndex + 1, endIndex);
+                }
+            }
+
+            int Partition(int[] arr, int startIndex, int endIndex)
+            {
+                // Use the first element as a pivot.
+                int pivotValue = arr[startIndex];
+                int temp;
+
+                // Index of the last element with a value less than the pivot value. 
+                int indexOfLastSmaller = startIndex;
+                for (int i = startIndex + 1; i <= endIndex; i++)
+                {
+                    // If current element is smaller than or equal to the pivot value.
+                    if (arr[i] <= pivotValue)
+                    {
+                        indexOfLastSmaller++;
+
+                        // Swap.
+                        temp = arr[indexOfLastSmaller];
+                        arr[indexOfLastSmaller] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+
+                // Place the pivot element at the correct position, after the last element smaller than the pivot value. 
+                temp = arr[indexOfLastSmaller];
+                arr[indexOfLastSmaller] = arr[startIndex];
+                arr[startIndex] = temp;
+
+                // Return the index of the pivot.
+                return indexOfLastSmaller;
+            }
+
+            #endregion
+        }
+
+        #endregion
+    }
+
+}
+
---
+++ NET.S.2019.Dremliug.01/ArraySortExtensionsTests/ArraySortExtensionsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Dremliug._01.ArraySortExtensionsTests
+{
+    class ArraySortExtensionsTests
+    {
+        #region SortTests
+
+        [TestCase(new[] { 4 }, new[] { 4 })]
+        [TestCase(new[] { 8, 4, 6, 3, 7 }, new[] { 3, 4, 6, 7, 8 })]
+        [TestCase(
+            new[] { -29, 44, 41, -9, -33, 33, 38, 46, -47, -34, -16, 40, -15, 24, 45, 47, 42, -14, 25, -6, 4, 5, 1, 44, -50, -44, 26, 31, -2, -26, -45, -39, 40, 45, 47, -34, -5, 2, 34, 12 },
+            new[] { -50, -47, -45, -44, -39, -34, -34, -33, -29, -26, -16, -15, -14, -9, -6, -5, -2, 1, 2, 4, 5, 12, 24, 25, 26, 31, 33, 34, 38, 40, 40, 41, 42, 44, 44, 45, 45, 46, 47, 47 })]
+
+        public void MergeSortTests(int[] array, int[] result)
+        {
+            array.MergeSort();
+            Assert.AreEqual(result, array);
+        }
+
+        [TestCase(new[] { 4 }, new[] { 4 })]
+        [TestCase(new[] { 8, 4, 6, 3, 7 }, new[] { 3, 4, 6, 7, 8 })]
+        [TestCase(
+            new[] { -29, 44, 41, -9, -33, 33, 38, 46, -47, -34, -16, 40, -15, 24, 45, 47, 42, -14, 25, -6, 4, 5, 1, 44, -50, -44, 26, 31, -2, -26, -45, -39, 40, 45, 47, -34, -5, 2, 34, 12 },
+            new[] { -50, -47, -45, -44, -39, -34, -34, -33, -29, -26, -16, -15, -14, -9, -6, -5, -2, 1, 2, 4, 5, 12, 24, 25, 26, 31, 33, 34, 38, 40, 40, 41, 42, 44, 44, 45, 45, 46, 47, 47 })]
+        public void QuickSortTests(int[] array, int[] result)
+        {
+            array.QuickSort();
+            Assert.AreEqual(result, array);
+        }
+
+        #endregion
+
+        #region NullArgumentTests
+
+        [TestCase(null)]
+        public void MergeSortArgumentIsNullTest(int[] array)
+        {
+            Assert.Throws<ArgumentNullException>(() => array.MergeSort());
+        }
+
+        [TestCase(null)]
+        public void QuickSortArgumentIsNullTest(int[] array)
+        {
+            Assert.Throws<ArgumentNullException>(() => array.QuickSort());
+        }
+
+        #endregion
+
+    }
+}
