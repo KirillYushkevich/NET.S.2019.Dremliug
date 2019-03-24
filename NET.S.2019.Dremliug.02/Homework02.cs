9d30d61357cced52800e7254477f476a6bfab426
deNoor <denoorx@gmail.com>
2019-03-24 19:58:43 +0300

    Day 02

---
+++ NET.S.2019.Dremliug.02/Homework02.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Dremliug._02
+{
+    public static class Homework02
+    {
+        #region InsertNumber
+
+        /// <summary> Copies bits of a <paramref name="source"/> number into [<paramref name="startOfBitRange"/>, <paramref name="endOfBitRange"/>] range of a <paramref name="target"/> number. </summary>
+        /// <param name="target"> An integer to insert bits into. </param>
+        /// <param name="source"> An integer to insert bits from. </param>
+        /// <param name="startOfBitRange"> The first position of a range. </param>
+        /// <param name="endOfBitRange"> The last position of a range. </param>
+        /// <returns> Modified target number. </returns>
+        public static int InsertNumber(int target, int source, int startOfBitRange, int endOfBitRange)
+        {
+            // Make sure that the range of bits corresponds to a 32-bit integer.
+            if (startOfBitRange < 0 || endOfBitRange < startOfBitRange || endOfBitRange > 31)
+            {
+                throw new ArgumentOutOfRangeException($"Bit positions must be in [0, 31] range; [{startOfBitRange}, {endOfBitRange}]");
+            }
+
+            // Create a bit mask of the required length.
+            var mask = (1 << (endOfBitRange - startOfBitRange + 1)) - 1;
+
+            // Take the required amount of bits from the source number.
+            var takenBitField = source & mask;
+
+            // Replace bits in the target number and return the result.
+            return (target & ~(mask << startOfBitRange)) | (takenBitField << startOfBitRange);
+        }
+
+        #endregion
+
+        #region FindNextBiggerNumber
+
+        /// <summary> Takes a positive integer number and returns the nearest largest integer consisting of the digits of the original number, -1 if no such number exists. </summary>
+        /// <param name="number"> A positive integer number. </param>
+        /// <returns> The nearest largest integer consisting of the digits of the original number; -1 if no such number exists. </returns>
+        public static int FindNextBiggerNumber(int number)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentOutOfRangeException("number", $"{number} must be positive");
+            }
+
+            // Next bigger number is guaranteed not exist.
+            if (number < 12)
+            {
+                return -1;
+            }
+
+            // Split the number to digits in the original order and store in a list. Lower positions of the digits have smaller indexes.
+            var digits = new List<int>(10);
+            int numberRemains = number;
+            for (int i = 0; numberRemains > 0; i++)
+            {
+                digits.Add(numberRemains % 10);
+                numberRemains /= 10;
+            }
+
+            // Search for a position that has a higher digit value than the digit in previous position.
+            int index;
+            for (index = 1; index < digits.Count; index++)
+            {
+                if (digits[index - 1] > digits[index]) break;
+            }
+
+            // If failed to find then NextBigger does not exist.
+            if (index == digits.Count)
+            {
+                return -1;
+            }
+
+            // Swap the digit at [index] with the digit at [index - 1].
+            (digits[index], digits[index - 1]) = (digits[index - 1], digits[index]);
+
+            // Sort digits by values at [0, index] range in descending order to make the new number as small as possible.
+            digits.Sort(0, index, Comparer<int>.Create((a, b) => b.CompareTo(a)));
+
+            // Build the new number.
+            int newNumber = 0;
+            for (int i = 0; i < digits.Count; i++)
+            {
+                try
+                {
+                    checked { newNumber += digits[i] * (int)Math.Pow(10, i); }
+                }
+                catch (OverflowException)
+                {
+                    // If overflow happens then int number does not exist.
+                    return -1;
+                }
+            }
+
+            return newNumber;
+        }
+
+        #endregion
+
+        #region TimeElapsed
+
+        /// <summary>
+        /// Displays an execution time of FindNextBiggerNumber method.
+        /// </summary>
+        /// <param name="number"> A number for FindNextBiggerNumber() method. </param>
+        /// <returns> Elapsed time as a string. </returns>
+        public static string TimeElapsed(int number)
+        {
+            // Warm-up is required.
+            FindNextBiggerNumber(number);
+
+            var watch = Stopwatch.StartNew();
+            FindNextBiggerNumber(number);
+            watch.Stop();
+
+            return watch.Elapsed.ToString();
+        }
+
+        #endregion
+
+        #region FilterDigit
+
+        /// <summary> Takes a list of integers and filters the list so that only numbers containing the specified digit are left at the output. </summary>
+        /// <param name="list"> A list of integers. </param>
+        /// <param name="digit"> A digit to search for. </param>
+        public static void FilterDigit(ref List<int> list, int digit)
+        {
+
+            #region Parameter validation
+
+            if (list is null)
+            {
+                throw new ArgumentNullException("list");
+            }
+
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("list", "list must contain elements");
+            }
+
+            if (digit > 9 || digit < 0)
+            {
+                throw new ArgumentOutOfRangeException("digit", $"digit must be in [0, 9] range; {digit}");
+            }
+
+            #endregion
+
+            var filteredList = new List<int>(list.Count);
+            var uniqueElements = new HashSet<int>(list.Count);
+            int currentElement;
+
+            // Check every digit of each number.
+            foreach (var v in list)
+            {
+                if (uniqueElements.Contains(v))
+                {
+                    continue;
+                }
+
+                currentElement = Math.Abs(v);
+                do
+                {
+                    if (currentElement % 10 == digit)
+                    {
+                        filteredList.Add(v);
+                        uniqueElements.Add(v);
+                        break;
+                    }
+                    currentElement /= 10;
+                } while (currentElement > 0);
+            }
+
+            // Set reference to the filtered list.
+            list = filteredList;
+        }
+
+        #endregion
+
+        #region FindNthRoot
+
+        /// <summary>
+        /// Calculates the nth root of a number by the Newton method with a given accuracy.
+        /// </summary>
+        /// <param name="number"> A number to calculate root from. </param>
+        /// <param name="degree"> A degree of the root. </param>
+        /// <param name="precision"> An accuracy of the result. </param>
+        /// <returns> <paramref name="Degree"/>-th root of the <paramref name="number"/> with the <paramref name="precision"/>. </returns>
+        public static double FindNthRoot(double number, int degree, double precision)
+        {
+
+            #region Parameter validation
+
+            if (double.IsNaN(number) || double.IsInfinity(number))
+            {
+                throw new ArgumentOutOfRangeException("number", $"number must be a real number; {number}");
+            }
+
+            if (degree < 1)
+            {
+                throw new ArgumentOutOfRangeException("degree", $"degree must be a natural number; {degree}");
+            }
+
+            if (precision >= 1 || precision <= 0 || double.IsInfinity(precision) || double.IsNaN(precision))
+            {
+                throw new ArgumentOutOfRangeException("precision", $"precision must be in (0, 1) range; {precision}");
+            }
+
+            #endregion
+
+            int numberOfFractionalDigits = 0;
+            while (precision < 1 && numberOfFractionalDigits < 16)
+            {
+                precision *= 10;
+                numberOfFractionalDigits++;
+            }
+
+            // A Double value has up to 15 decimal digits of precision. 
+            if (numberOfFractionalDigits > 15)
+            {
+                throw new ArgumentOutOfRangeException("precision", $"precision limit is 15 fractional digits; {precision}");
+            }
+
+            double x0;
+            // Guess an initial value of x1.
+            double x1 = 1;
+            do
+            {
+                x0 = x1;
+                x1 += (number / Math.Pow(x1, degree - 1.0) - x1) / degree;
+            }
+            // (10 ^ -16) difference is the precision limit. Further computations won't change x1.
+            // If an endless loop happens, decrease to (10 ^ -15).
+            while (Math.Abs(x0 - x1) > Math.Pow(10, -16));
+
+            // Round to the required precision.
+            return Math.Round(x1, numberOfFractionalDigits);
+
+        }
+
+        #endregion
+    }
+}
---
+++ NET.S.2019.Dremliug.02/Tests/Homework02Tests.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NET.S._2019.Dremliug._02.Tests
+{
+    [TestFixture]
+    class Homework02Tests
+    {
+
+        #region InsertNumber
+
+        [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        [TestCase(8, 15, 3, 8, ExpectedResult = 120)]
+        [TestCase(-819436951, 1839623026, 7, 26, ExpectedResult = -885671575)]
+        public int InsertNumberTests(int target, int source, int startOfBitRange, int endOfBitRange)
+            => Homework02.InsertNumber(target, source, startOfBitRange, endOfBitRange);
+
+        [TestCase(8, 15, 8, 3)]
+        [TestCase(8, 15, -1, 8)]
+        [TestCase(8, 15, 3, 32)]
+        [TestCase(8, 15, 8, 3)]
+        public void InsertNumberTests_WrongBitPositions_ThrowsArgumentOutOfRangeException(int target, int source, int startOfBitRange, int endOfBitRange)
+            => Assert.Throws<ArgumentOutOfRangeException>(() => Homework02.InsertNumber(target, source, startOfBitRange, endOfBitRange));
+
+        #endregion
+
+        #region FindNextBiggerNumber
+
+        [TestCase(12, ExpectedResult = 21)]
+        [TestCase(513, ExpectedResult = 531)]
+        [TestCase(2017, ExpectedResult = 2071)]
+        [TestCase(414, ExpectedResult = 441)]
+        [TestCase(144, ExpectedResult = 414)]
+        [TestCase(1234321, ExpectedResult = 1241233)]
+        [TestCase(1234126, ExpectedResult = 1234162)]
+        [TestCase(3456432, ExpectedResult = 3462345)]
+        [TestCase(10, ExpectedResult = -1)]
+        [TestCase(20, ExpectedResult = -1)]
+        public int FindNextBiggerNumberTests(int number)
+            => Homework02.FindNextBiggerNumber(number);
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void FindNextBiggerNumberTests_NonPositiveNumber_ThrowsArgumentOutOfRangeException(int number)
+            => Assert.Throws<ArgumentOutOfRangeException>(() => Homework02.FindNextBiggerNumber(number));
+
+        #endregion
+
+        #region TimeElapsed
+
+        // How to find out if the return time value is correct or not?
+        [TestCase(3456432)]
+        public void TimeElapsedTests(int number)
+            => Assert.IsInstanceOf<string>(Homework02.TimeElapsed(number));
+
+        #endregion
+
+        #region FilterDigit
+
+        private static IEnumerable<TestCaseData> DataCasesForFilterDigit
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new List<int> { 7, 70, 17 },
+                    new List<int> { 7, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 },
+                    7);
+                yield return new TestCaseData(
+                    new List<int> { -46, -84, 44, 4, 46, 48 },
+                    new List<int> { -5, -46, -84, 1, 44, 2, 3, 4, 5, 46, 7, 48, 68, 69, 70, 4, 17 },
+                    4);
+                yield return new TestCaseData(
+                    new List<int> { 0, -40, 10, 2000, 304, 7028, -304 },
+                    new List<int> { -5, 0, -40, -84, 10, 44, 2000, 304, 4, 5, 46, 7, 0, 68, 69, 7028, -304, 4, 17 },
+                    0);
+            }
+        }
+
+        [TestCaseSource(nameof(DataCasesForFilterDigit))]
+        public void FilterDigitTests(List<int> expected, List<int> data, int digit)
+        {
+            Homework02.FilterDigit(ref data, digit);
+            Assert.AreEqual(expected, data);
+        }
+
+        [Test]
+        public void FilterDigitTests_NullList_ThrowsArgumentNullException()
+        {
+            List<int> data = null;
+            int digit = 7;
+
+            Assert.Throws<ArgumentNullException>(() => Homework02.FilterDigit(ref data, digit));
+        }
+
+        [Test]
+        public void FilterDigitTests_EmptyList_ThrowsArgumentException()
+        {
+            List<int> data = new List<int>();
+            int digit = 7;
+
+            Assert.Throws<ArgumentException>(() => Homework02.FilterDigit(ref data, digit));
+        }
+
+        [TestCase(11)]
+        [TestCase(-1)]
+        public void FilterDigitTests_WrongDigit_ThrowsArgumentOutOfRangeException(int digit)
+        {
+            var data = new List<int> { 7, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => Homework02.FilterDigit(ref data, digit));
+        }
+
+        #endregion
+
+        #region FindNthRoot
+
+        [TestCase(1, 5, 0.0001, ExpectedResult = 1)]
+        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
+        [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]
+        [TestCase(-0.001, 3, 0.0001, ExpectedResult = -0.1)]
+        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)]
+        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
+        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)]
+        [TestCase(-0.0279936, 7, 0.0001, ExpectedResult = -0.6)]
+        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)]
+        [TestCase(-0.008, 3, 0.1, ExpectedResult = -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)]
+        public double FindNthRootTests(double number, int degree, double precision)
+        {
+            return Homework02.FindNthRoot(number, degree, precision);
+        }
+
+        [TestCase(8, 15, -7)]
+        [TestCase(8, 15, -0.6)]
+        [TestCase(0.004241979, 9, 0)]
+        [TestCase(0.004241979, 9, 0.0000000000000001)]
+        [TestCase(0.004241979, -1, 0.0001)]
+        [TestCase(0.004241979, 9, 1)]
+        [TestCase(0.004241979, 9, 1.3)]
+        [TestCase(0.004241979, 9, -0.0001)]
+        [TestCase(double.NaN, 9, 0.0001)]
+        [TestCase(double.PositiveInfinity, 9, 0.0001)]
+        [TestCase(0.004241979, 9, double.NaN)]
+        [TestCase(0.004241979, 9, double.NegativeInfinity)]
+        public void FindNthRootTests_ThrowsOutOfRangeExceptions(double number, int degree, double precision)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Homework02.FindNthRoot(number, degree, precision));
+        }
+
+        #endregion
+    }
+}
