8e78a2c351e24b7bf5e337f5d20f361edf6e6b47
deNoor <denoorx@gmail.com>
2019-03-29 00:51:45 +0300

    Day04 double extension.

---
+++ NET.S.2019.Dremliug.04/Readme.md
@@ -0,0 +1,7 @@
+1.
+  - Create a class that allows you to perform GCD calculations using the [*Euclidean algorithm*](http://en.wikipedia.org/wiki/Euclidean_algorithm) for two or more integers.
+  - Add to the created class methods that implement the [*Stein algorithm*](http://en.wikipedia.org/wiki/Binary_GCD_algorithm) (binary Euclidean algorithm).
+  - Class methods in addition to calculating the GCD should measure the time required to perform the calculation.
+  - Useful link: [[RU]](https://habrahabr.ru/post/205106/)
+
+2. Implement an extension method to get a string binary representation of a real double-precision number in IEEE 754 format (it is desirable not to use ready-made converter classes).
---
+++ NET.S.2019.Dremliug.04/Tests/DoubleExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NET.S._2019.Dremliug._04
+{
+    [TestFixture]
+    class DoubleExtensionsTests
+    {
+        [TestCase(-255.255)]
+        [TestCase(255.255)]
+        [TestCase(4294967295.0)]
+        [TestCase(double.MinValue)]
+        [TestCase(double.MaxValue)]
+        [TestCase(double.Epsilon)]
+        [TestCase(double.NaN)]
+        [TestCase(double.NegativeInfinity)]
+        [TestCase(double.PositiveInfinity)]
+        [TestCase(-0.0)]
+        [TestCase(0.0)]
+        public void ToBinaryStringTests(double number)
+        {
+            string str = Convert.ToString(BitConverter.DoubleToInt64Bits(number), 2);
+            string precedingZeroes = new string('0', 64 - str.Length);
+            string expected = precedingZeroes + str;
+
+            string actual = number.ToBinaryString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(-255.255, ExpectedResult = "1100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(255.255, ExpectedResult = "0100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(4294967295.0, ExpectedResult = "0100000111101111111111111111111111111111111000000000000000000000")]
+        [TestCase(double.MinValue, ExpectedResult = "1111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.MaxValue, ExpectedResult = "0111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.Epsilon, ExpectedResult = "0000000000000000000000000000000000000000000000000000000000000001")]
+        [TestCase(double.NaN, ExpectedResult = "1111111111111000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.NegativeInfinity, ExpectedResult = "1111111111110000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.PositiveInfinity, ExpectedResult = "0111111111110000000000000000000000000000000000000000000000000000")]
+        [TestCase(-0.0, ExpectedResult = "1000000000000000000000000000000000000000000000000000000000000000")]
+        [TestCase(0.0, ExpectedResult = "0000000000000000000000000000000000000000000000000000000000000000")]
+        public string ToBinaryStringTests_HardcodedResults(double number)
+            => number.ToBinaryString();
+
+    }
+}
