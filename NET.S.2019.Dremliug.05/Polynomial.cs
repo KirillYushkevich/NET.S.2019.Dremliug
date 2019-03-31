05e091e23bcabca713a527d2d79bfea1f451982c
deNoor <denoorx@gmail.com>
2019-04-01 01:23:35 +0300

    Day 05. Polynominal added.

---
+++ NET.S.2019.Dremliug.05/Polynomial.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Dremliug._05
+{
+    /// <summary> Floating-point polynomial in one variable. </summary>
+    public sealed class Polynomial : IEquatable<Polynomial>
+    {
+        private readonly double[] _coeffs;
+
+        #region Constructor
+        /// <summary>
+        /// Creates a polynomial with given coefficients.
+        /// </summary>
+        /// <param name="coeffs"> Polynomial coefficients. </param>
+        public Polynomial(params double[] coeffs)
+        {
+            if (coeffs is null)
+            {
+                throw new ArgumentNullException($"{coeffs} must not be null");
+            }
+            if (coeffs.Length == 0)
+            {
+                throw new ArgumentException($"{coeffs} must have at least one argument");
+            }
+
+            // Get the highers degree of polynomial.
+            int highestDegreeWithNonZeroCoeff = 0;
+            for (int i = coeffs.Length - 1; i >= 0; i--)
+            {
+                if (coeffs[i] != 0)
+                {
+                    highestDegreeWithNonZeroCoeff = i;
+                    break;
+                }
+            }
+
+            // Create a polynomial of the degree found.
+            _coeffs = new double[highestDegreeWithNonZeroCoeff + 1];
+            for (int i = 0; i < _coeffs.Length; i++)
+            {
+                _coeffs[i] = coeffs[i];
+            }
+
+            // Set the degree of polynomial.
+            Degree = highestDegreeWithNonZeroCoeff;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// The degree of polynomial.
+        /// </summary>
+        public int Degree { get; }
+        #endregion
+
+        #region Equality
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Polynomial);
+        }
+
+        public bool Equals(Polynomial other)
+        {
+            return other != null &&
+                   this.ToArray().SequenceEqual(other.ToArray());
+        }
+
+        public override int GetHashCode()
+        {
+            return ((IStructuralEquatable)_coeffs).GetHashCode(EqualityComparer<double>.Default);
+        }
+
+        public static bool operator ==(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            return ReferenceEquals(polynomial1, polynomial2) ||
+                (polynomial1?.ToArray().SequenceEqual(polynomial2?.ToArray() ?? Enumerable.Empty<double>()) ?? false);
+        }
+
+        public static bool operator !=(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            return !(polynomial1 == polynomial2);
+        }
+        #endregion
+
+        #region ToString
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int length = _coeffs.Length;
+
+            sb.Append($"{_coeffs[length - 1]}*x^{length - 1} ");
+
+            for (int i = length - 2; i >= 2; i--)
+            {
+                if (_coeffs[i] != 0)
+                {
+                    sb.Append($"{withSign(_coeffs[i])}*x^{i} ");
+                }
+            }
+
+            if (_coeffs[1] != 0)
+            {
+                sb.Append($"{withSign(_coeffs[1])}*x ");
+            }
+            if (_coeffs[0] != 0)
+            {
+                sb.Append($"{withSign(_coeffs[0])}");
+            }
+
+            return sb.ToString().Trim();
+
+            string withSign(double value)
+            {
+                return value > 0 ? $"+ {value}" : $"- {-value}";
+            }
+        }
+        #endregion
+
+        public double[] ToArray() => _coeffs.ToArray();
+
+        #region overloaded operators
+        public static Polynomial operator +(Polynomial left, Polynomial right)
+        {
+            double[] leftA = left.ToArray();
+            double[] rightA = right.ToArray();
+
+            // Zip both arrays + remaining elements of the left if any + remaining elements of the right if any.
+            return new Polynomial(leftA.Zip(rightA, (x, y) => x + y)
+                                        .Concat(leftA.Skip(rightA.Length))
+                                        .Concat(rightA.Skip(leftA.Length))
+                                        .ToArray());
+        }
+
+        public static Polynomial operator -(Polynomial left, Polynomial right)
+        {
+            double[] leftA = left.ToArray();
+            double[] rightA = right.ToArray();
+
+            // Zip both arrays + remaining elements of the left if any + remaining elements of the right multiplied by -1 if any.
+            return new Polynomial(leftA.Zip(rightA, (x, y) => x - y)
+                                        .Concat(leftA.Skip(rightA.Length))
+                                        .Concat(rightA.Skip(leftA.Length).Select((x) => -x))
+                                        .ToArray());
+        }
+
+        public static Polynomial operator *(Polynomial left, Polynomial right)
+        {
+            double[] leftA = left.ToArray();
+            double[] rightA = right.ToArray();
+            double[] result = new double[leftA.Length + rightA.Length - 1];
+
+            for (int i = 0; i < leftA.Length; i++)
+            {
+                for (int j = 0; j < rightA.Length; j++)
+                {
+                    result[i + j] += leftA[i] * rightA[j];
+                }
+            }
+
+            return new Polynomial(result);
+        }
+        #endregion
+    }
+}
---
+++ NET.S.2019.Dremliug.05/Tests/PolynomialTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NET.S._2019.Dremliug._05.Tests
+{
+    [TestFixture]
+    class PolynomialTests
+    {
+        #region Exceptions Tests
+        [Test]
+        public void Throws_ArgExc() =>
+    Assert.Throws<ArgumentException>(() => new Polynomial());
+
+        [Test]
+        public void Throws_ArgNullExc() =>
+            Assert.Throws<ArgumentNullException>(() => new Polynomial(null)); 
+        #endregion
+
+        #region Equality Tests
+        [Test]
+        public void EqualsForTheSameObject()
+        {
+            Polynomial a = new Polynomial(14.0);
+            Polynomial b = a;
+
+            Assert.IsTrue(a == b);
+            Assert.IsTrue(b == a);
+        }
+
+        [Test]
+        public void EqualsWithOneNull()
+        {
+            Polynomial a = new Polynomial(14.0);
+            Polynomial b = null;
+
+            Assert.IsFalse(a == b);
+            Assert.IsFalse(b == a);
+        }
+
+        [Test]
+        public void EqualsWithTwoNull()
+        {
+            Polynomial a = null;
+            Polynomial b = null;
+
+            Assert.IsTrue(a == b);
+            Assert.IsTrue(b == a);
+        }
+
+        [Test]
+        public void EqualsWithTwoNonNull()
+        {
+            Polynomial a = new Polynomial(14.0, -1.2, 8.1);
+            Polynomial b = new Polynomial(14.0, -1.2, 8.1);
+
+            Assert.IsTrue(a == b);
+            Assert.IsTrue(b == a);
+        }
+
+        [TestCase(new double[] { 1d, 2d, 3d }, new double[] { 1d, 2d, 3d }, ExpectedResult = true)]
+        [TestCase(new double[] { 1d, 2d, 3d, 0d }, new double[] { 1d, 2d, 3d }, ExpectedResult = true)]
+        [TestCase(new double[] { 1d, 2d }, new double[] { 1d, 2d, 3d }, ExpectedResult = false)]
+        [TestCase(new double[] { 1d, 2d }, new double[] { 2d, 3d }, ExpectedResult = false)]
+        public bool Overrided_Equals_Is_Correct(double[] left, double[] right)
+        {
+            Polynomial leftP = new Polynomial(left);
+            Polynomial rightP = new Polynomial(right);
+
+            return leftP.Equals(rightP);
+        }
+        #endregion
+
+        #region HashCode Tests
+        [Test]
+        public void GetHashCodeTest()
+        {
+            Polynomial a = new Polynomial(new double[] { 14.1 });
+            Polynomial b = new Polynomial(new double[] { 14.1 });
+            Polynomial c = a;
+
+            HashSet<Polynomial> hs = new HashSet<Polynomial>(new Polynomial[] { a, b, c });
+
+            Assert.AreEqual(1, hs.Count);
+        }
+        #endregion
+
+        #region Operators
+        [TestCase(new double[] { 1d, 2d }, new double[] { 1d, -3d, 8d }, new double[] { 2d, -1d, 8d })]
+        [TestCase(new double[] { 15.5, -27.1, 0.0, 0.0, 345.223 }, new double[] { 35, 0.0, 1, 56 }, new double[] { 50.5, -27.1, 1.0, 56, 345.223 })]
+        public void PlusOperator(double[] left, double[] right, double[] expectedResult)
+        {
+            Polynomial leftP = new Polynomial(left);
+            Polynomial rightP = new Polynomial(right);
+            Polynomial expected = new Polynomial(expectedResult);
+
+            Polynomial actual = leftP + rightP;
+
+            Assert.IsTrue(actual.ToArray().SequenceEqual(expected.ToArray()));
+        }
+
+        [TestCase(new double[] { 1d, 2d }, new double[] { 1d, 2d, 3d }, new double[] { 0d, 0d, -3d })]
+        [TestCase(
+            new double[] { 15.5, -27.1, 0.0, 0.0, 345.223 },
+            new double[] { 35, 0.0, 1, 56 },
+            new double[] { -19.5, -27.1, -1.0, -56.0, 345.223 })]
+        public void MinusOperator(double[] left, double[] right, double[] expectedResult)
+        {
+            Polynomial leftP = new Polynomial(left);
+            Polynomial rightP = new Polynomial(right);
+            Polynomial expected = new Polynomial(expectedResult);
+
+            Polynomial actual = leftP - rightP;
+
+            Assert.IsTrue(actual.ToArray().SequenceEqual(expected.ToArray()));
+        }
+
+        [TestCase(new double[] { 1d, 2d }, new double[] { 1d, 2d, 3d }, new double[] { 1d, 4d, 7d, 6d })]
+        [TestCase(
+            new double[] { 15.5, -27.1, 0.0, 0.0, 345.223 },
+            new double[] { 35, 0.0, 1, 56 },
+            new double[] { 542.5, -948.5, 15.5, 840.9, 10565.205, 0.0, 345.223, 19332.488 })]
+        public void MulOperator(double[] left, double[] right, double[] expectedResult)
+        {
+            Polynomial leftP = new Polynomial(left);
+            Polynomial rightP = new Polynomial(right);
+            Polynomial expected = new Polynomial(expectedResult);
+
+            Polynomial actual = leftP * rightP;
+
+            Assert.IsTrue(actual.ToArray().SequenceEqual(expected.ToArray()));
+        }
+        #endregion
+
+        [TestCase(new double[] { 1d, 2d, 3d }, ExpectedResult = "3*x^2 + 2*x + 1")]
+        [TestCase(new double[] { 0d, 2d, 3d }, ExpectedResult = "3*x^2 + 2*x")]
+        //[TestCase(new double[] { 1d, 2d }, ExpectedResult = "2*x + 1")]
+        [TestCase(new double[] { 0.0, -1, -1.0, -56.0, 345.223 }, ExpectedResult = "345.223*x^4 - 56*x^3 - 1*x^2 - 1*x")]
+        public string ToStringTest(double[] arr)
+        {
+            Polynomial polynomial = new Polynomial(arr);
+
+            return polynomial.ToString();
+        }
+    }
+}
