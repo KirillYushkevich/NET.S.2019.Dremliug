3c8aee2dbd077c0110f1796eae31f0c9ee4a9f69
deNoor <denoorx@gmail.com>
2019-04-07 23:55:53 +0300

    bank account drafts

---
+++ NET.S.2019.Dremliug.08/BankAccount/AccountControlService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class AccountControlService
+    {
+        private HashSet<AccountTemplate> _setOfAccounts;
+
+        public void Deposit(decimal amount)
+        {
+
+        }
+
+        public void Withdraw(decimal amount)
+        {
+
+        }
+
+        public AccountTemplate CreateNew()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
